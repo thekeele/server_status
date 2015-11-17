ae6d6a265b5950f3d1464fd031685b3d6dbe0c69
Mark Keele <mkeele@indiana.edu>
2015-11-17 18:38:37 -0500

    add CORS support for dev

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,3 +13,11 @@ angular
     'ngAnimate',
     'ngResource'
   ]);
+
+angular.module('serverStatusApp').config(function($httpProvider) {
+  //Enable cross domain calls
+  $httpProvider.defaults.useXDomain = true;
+
+  //Remove the header used to identify ajax call  that would prevent CORS from working
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});

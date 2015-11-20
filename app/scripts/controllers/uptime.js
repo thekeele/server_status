a2ee7fe828dbbac878d541d4e4c67115b6a1972a
Mark Keele <mkeele@indiana.edu>
2015-11-20 18:41:14 -0500

    refactor uptime div

--- app/scripts/controllers/uptime.js
+++ app/scripts/controllers/uptime.js
@@ -11,13 +11,10 @@ angular.module('serverStatusApp')
 
     // getUptime() returns a promise so use then for processing
     UptimeService.getUptime().then(function(uptimeData) {
-
-      // capitalize the u because pretty
-      var uptime = uptimeData.uptime.replace('u', 'U');
-      $scope.uptime = uptime;
+      $scope.uptime = uptimeData.uptime;
 
       // if the system is up for less than 24 hrs show warning
-      var day = uptime.search('day');
+      var day = uptimeData.uptime.search('day');
       $scope.day = function () {
         if (day === -1)
           return 'warning';
@@ -25,7 +22,6 @@ angular.module('serverStatusApp')
           return 'success';
       }
 
-      var last_reboot = 'Last Reboot @ ' + uptimeData.last_reboot;
-      $scope.last_reboot = last_reboot;
+      $scope.last_reboot = 'Last Reboot ' + uptimeData.last_reboot + ' EST';
     });
   });

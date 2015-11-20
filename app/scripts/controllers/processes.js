f10cab322d524185864acf444909ea82773f5073
Mark Keele <mkeele@indiana.edu>
2015-11-20 18:41:50 -0500

    refactor processes div

--- app/scripts/controllers/processes.js
+++ app/scripts/controllers/processes.js
@@ -11,8 +11,42 @@ angular.module('serverStatusApp')
 
     // getProcesses() returns a promise so use then for processing
     ProcessesService.getProcesses().then(function(processesData) {
-      $scope.nginx = processesData.nginx
-      $scope.blog = processesData.blog
-      $scope.lux = processesData.lux
+      var nginx = processesData.nginx;
+      var blog = processesData.blog;
+      var lux = processesData.lux;
+
+      $scope.nginxStatus = function () {
+        if (nginx.match(/running/g) == "running") {
+          return 'success';
+        } else if (nginx.match(/waiting/g) == "waiting") {
+          return 'warning';
+        } else {
+          return 'danger';
+        }
+      };
+
+      $scope.blogStatus = function () {
+        if (blog.match(/running/g) == "running") {
+          return 'success';
+        } else if (blog.match(/waiting/g) == "waiting") {
+          return 'warning';
+        } else {
+          return 'danger';
+        }
+      };
+
+      $scope.luxStatus = function () {
+        if (lux.match(/running/g) == "running") {
+          return 'success';
+        } else if (lux.match(/waiting/g) == "waiting") {
+          return 'warning';
+        } else {
+          return 'danger';
+        }
+      };
+
+      $scope.nginx = nginx;
+      $scope.blog = blog;
+      $scope.lux = lux;
     });
   });

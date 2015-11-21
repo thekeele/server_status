b0d88c58224caa31f8bc97ebc1ff22aa1b3e1566
Mark Keele <mkeele@indiana.edu>
2015-11-21 18:02:39 -0500

    refactor alerts controller/view

--- app/scripts/controllers/alerts.js
+++ app/scripts/controllers/alerts.js
@@ -11,11 +11,10 @@ angular.module('serverStatusApp')
 
     // getAlerts() returns a promise so use then for processing
     AlertsService.getAlerts().then(function(alertsData) {
-      var ban = alertsData.ssh.cur_ban;
-      var fail = alertsData.ssh.total_fail;
-      console.log(fail);
-
-      $scope.sshLevel = function () {
+      $scope.getLevel = function (alert_type) {
+        var active_alert = false;
+        var ban = alertsData[alert_type].cur_ban;
+        var fail = alertsData[alert_type].total_fail;
         var level = '';
 
         if (fail < 0) {
@@ -29,10 +28,17 @@ angular.module('serverStatusApp')
         if (ban > 0) {
           level = 'danger';
         }
+
+        if (fail > 0) {
+          active_alert = true;
+        }
+
+        $scope.active_alert = active_alert;
+        $scope.alert_type = alert_type;
+        $scope.alert_ban = 'Currently banned: ' + ban;
+        $scope.alert_fail = 'Total failures: ' + fail;
+
         return level;
       };
-
-      $scope.ssh_ban = 'Currently banned: ' + ban;
-      $scope.ssh_fail = 'Total failures: ' + fail;
     });
   });

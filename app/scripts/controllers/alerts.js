6b65d2bc49f4dc40b9b36a1ca18da5e9e9eac36a
Mark Keele <mkeele@indiana.edu>
2015-11-21 12:43:45 -0500

    alert display template

--- app/scripts/controllers/alerts.js
+++ app/scripts/controllers/alerts.js
@@ -11,7 +11,28 @@ angular.module('serverStatusApp')
 
     // getAlerts() returns a promise so use then for processing
     AlertsService.getAlerts().then(function(alertsData) {
-      $scope.ssh = 'SSH: ' + alertsData.ssh;
-      $scope.noscript = 'Noscript: ' + alertsData.noscript;
+      var ban = alertsData.ssh.cur_ban;
+      var fail = alertsData.ssh.total_fail;
+      console.log(fail);
+
+      $scope.sshLevel = function () {
+        var level = '';
+
+        if (fail < 0) {
+          level = 'success';
+        }
+
+        if (fail > 0) {
+          level = 'warning';
+        }
+
+        if (ban > 0) {
+          level = 'danger';
+        }
+        return level;
+      };
+
+      $scope.ssh_ban = 'Currently banned: ' + ban;
+      $scope.ssh_fail = 'Total failures: ' + fail;
     });
   });

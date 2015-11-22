553c2bd1ae592a0924a89ff2cc86de5406ba681d
Mark Keele <mkeele@indiana.edu>
2015-11-22 12:42:32 -0500

    service data in scope for ng-repeat, explicit type comparison

--- app/scripts/controllers/processes.js
+++ app/scripts/controllers/processes.js
@@ -11,42 +11,21 @@ angular.module('serverStatusApp')
 
     // getProcesses() returns a promise so use then for processing
     ProcessesService.getProcesses().then(function(processesData) {
-      var nginx = processesData.nginx;
-      var blog = processesData.blog;
-      var lux = processesData.lux;
 
-      $scope.nginxStatus = function () {
-        if (nginx.match(/running/g) == "running") {
+      $scope.processes = processesData;
+
+      $scope.processStatus = function(process) {
+        var running = process.match(/running/g);
+        var waiting = process.match(/waiting/g);
+
+        if (running != null && running.toString() === "running") {
           return 'success';
-        } else if (nginx.match(/waiting/g) == "waiting") {
+        } else if (waiting != null && waiting.toString() === "waiting") {
           return 'warning';
         } else {
           return 'danger';
         }
       };
 
-      $scope.blogStatus = function () {
-        if (blog.match(/running/g) == "running") {
-          return 'success';
-        } else if (blog.match(/waiting/g) == "waiting") {
-          return 'warning';
-        } else {
-          return 'danger';
-        }
-      };
-
-      $scope.luxStatus = function () {
-        if (lux.match(/running/g) == "running") {
-          return 'success';
-        } else if (lux.match(/waiting/g) == "waiting") {
-          return 'warning';
-        } else {
-          return 'danger';
-        }
-      };
-
-      $scope.nginx = nginx;
-      $scope.blog = blog;
-      $scope.lux = lux;
     });
   });

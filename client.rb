a242d25a513fce50a78314a04468863c698eea23
Mark Keele <m.keele88@gmail.com>
2016-03-24 20:27:05 -0400

    not needed, just for testing

--- client.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-require 'rest-client'
-
-response = RestClient.get('http://localhost:9000')
-puts response
-
-response = RestClient.get('http://localhost:9000/hiphop')
-puts response

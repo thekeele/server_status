6068c310e03c62d68e2186325c6edc582847105d
Mark Keele <m.keele88@gmail.com>
2016-06-02 18:21:30 -0400

    spacing

--- server/api.rb
+++ server/api.rb
@@ -7,41 +7,40 @@ require 'json'
 require_relative 'status'
 
 class StatusAPI < Sinatra::Base
+  configure :development, :test do
+    enable :logging
+  end
 
-configure :development, :test do
-  enable :logging
-end
+  status = Status.new()
 
-status = Status.new()
+  before do
+    content_type :json
 
-before do
-  content_type :json
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET']
+  end
 
-  headers 'Access-Control-Allow-Origin' => '*',
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET']
-end
+  get '/' do
+    JSON.pretty_generate({:uptime => "#{request.base_url}/uptime",
+                          :vitals => "#{request.base_url}/vitals",
+                          :processes => "#{request.base_url}/processes",
+                          :alerts => "#{request.base_url}/alerts"})
+  end
 
-get '/' do
-  JSON.pretty_generate({:uptime => "#{request.base_url}/uptime",
-                        :vitals => "#{request.base_url}/vitals",
-                        :processes => "#{request.base_url}/processes",
-                        :alerts => "#{request.base_url}/alerts"})
-end
+  get '/uptime' do
+    JSON.pretty_generate({:uptime => status.uptime,
+                          :last_reboot => status.last_reboot})
+  end
 
-get '/uptime' do
-  JSON.pretty_generate({:uptime => status.uptime,
-                        :last_reboot => status.last_reboot})
-end
+  get '/vitals' do
+    JSON.pretty_generate(status.vitals)
+  end
 
-get '/vitals' do
-  JSON.pretty_generate(status.vitals)
-end
+  get '/processes' do
+    JSON.pretty_generate(status.processes)
+  end
 
-get '/processes' do
-  JSON.pretty_generate(status.processes)
-end
-
-get '/alerts' do
-  JSON.pretty_generate(status.alerts)
-end
+  get '/alerts' do
+    JSON.pretty_generate(status.alerts)
+  end
 end

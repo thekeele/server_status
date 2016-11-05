cacf6ad308f00bf619ab5bc8f7f2085611d49594
Mark Keele <mark@keele.me>
2016-11-05 14:53:35 -0600

    api handles env, loads routes, runs

--- server/api.rb
+++ server/api.rb
@@ -1,46 +1,63 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
-require 'sinatra/config_file'
-require 'sinatra/reloader' if development?
-require 'json'
-require_relative 'status'
+## use RACK_ENV environment or default to development
+ENV['RACK_ENV'] ||= 'development'
 
+# sinatra / ruby libs, sinatra/base required for modular style
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'sinatra/reloader'
+require 'json'
+
+# relative paths to user written libs
+require_relative 'routes/routes'
+
+# modular style app, inherits from Sinatra Base
 class StatusAPI < Sinatra::Base
+  # register and load yaml config file
+  register Sinatra::ConfigFile
+  config_file 'config.yml'
+
+  # set the root dir of the app
+  set :root, settings.root
+
+  # development, test specific settings
   configure :development, :test do
     enable :logging
+    register Sinatra::Reloader
+
+    # loaded from config.yml
+    set :bind, settings.development[:bind]
+    set :port, settings.development[:port]
+    set :run, settings.development[:run]
+    set :server, settings.development[:server]
   end
 
-  status = Status.new()
+  # production specific settings
+  configure :production do
+    enable :logging
 
-  before do
-    content_type :json
+    # loaded from config.yml
+    set :bind, settings.production[:bind]
+    set :port, settings.production[:port]
+    set :run, settings.production[:run]
+    set :server, settings.production[:server]
 
-    headers 'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET']
+    def self.run!
+      super do |server|
+        server.ssl = true
+        server.ssl_options = {
+          :cert_chain_file => File.dirname(__FILE__) + "",
+          :private_key_file => File.dirname(__FILE__) + "",
+          :verify_peer => false
+        }
+      end
+    end
   end
 
-  get '/' do
-    JSON.pretty_generate({:uptime => "#{request.base_url}/uptime",
-                          :vitals => "#{request.base_url}/vitals",
-                          :processes => "#{request.base_url}/processes",
-                          :alerts => "#{request.base_url}/alerts"})
-  end
+  # register Routes module with Sinatra
+  register Sinatra::StatusAPI::Routes
 
-  get '/uptime' do
-    JSON.pretty_generate({:uptime => status.uptime,
-                          :last_reboot => status.last_reboot})
-  end
-
-  get '/vitals' do
-    JSON.pretty_generate(status.vitals)
-  end
-
-  get '/processes' do
-    JSON.pretty_generate(status.processes)
-  end
-
-  get '/alerts' do
-    JSON.pretty_generate(status.alerts)
-  end
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
